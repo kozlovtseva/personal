fcb07677fd3e49cfff32f63fc904543893b1e4f5
Kozlovtseva <alina.lm@mail.ru>
2019-11-28 10:59:27 +0300

    fill in json

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const Layout: React.FC<{}> = () => {
     return (
         <div>
+            <Header />
             <h1>My App</h1>
+            <Footer />
         </div>
     );
 };
