22e53c71064cf144ce5e5cc287639f7ea9313467
Kozlovtseva <alina.lm@mail.ru>
2020-01-10 22:10:29 +0300

    add elements to footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,12 +6,36 @@ import styles from './Footer.module.scss';
 const Footer: React.FC<{}> = () => {
     return (
         <div className={styles.Container}>
-            <p>Задизайнено собственными ручками в Figma</p>
-            <Button
-                classesNames="Button Button_Yellow"
-                href="https://www.figma.com/"
-                text="посмотреть"
-            />
+            <div className={styles.Design}>
+                <p>Задизайнено собственными ручками в Figma</p>
+                <Button
+                    classesNames="Button Button_Yellow"
+                    href="https://www.figma.com/file/L9ov2PqvFTtNATDYW35Yyq/personal?node-id=0%3A1"
+                    text="посмотреть"
+                />
+            </div>
+            <div className={styles.Technologies}>
+                <h5 className={styles.Title}>Что использовала:</h5>
+                <ul className={styles.List}>
+                    <li className={styles.Item}>
+                        React & Redux (каркас Create React App)
+                    </li>
+                    <li className={styles.Item}>TypeScript</li>
+                    <li className={styles.Item}>React Router</li>
+                    <li className={styles.Item}>SASS(SCSS)</li>
+                    <li className={styles.Item}>CSS Modules</li>
+                </ul>
+            </div>
+            <div className={styles.GitHub}>
+                <p>Исходный код (GitHub)</p>
+                <div className={styles.Button}>
+                    <Button
+                        classesNames="Button Button_Yellow"
+                        href="https://github.com/kozlovtseva/personal"
+                        text="посмотреть"
+                    />
+                </div>
+            </div>
         </div>
     );
 };
