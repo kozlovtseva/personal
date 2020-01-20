89a8ca5f7b96e831df605578ec348e30940a7cc4
Kozlovtseva <alina.lm@mail.ru>
2020-01-20 12:05:41 +0300

    fix styles for footer and navigation

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,36 +5,36 @@ import styles from './Footer.module.scss';
 
 const Footer: React.FC<{}> = () => {
     return (
-        <div className={styles.Container}>
-            <div className={styles.Design}>
-                <p>Задизайнено собственными ручками в Figma</p>
-                <Button
-                    classesNames="Button Button_Yellow"
-                    href="https://www.figma.com/file/L9ov2PqvFTtNATDYW35Yyq/personal?node-id=0%3A1"
-                    text="посмотреть"
-                />
-            </div>
-            <div className={styles.Technologies}>
-                <h5 className={styles.Title}>Что использовала:</h5>
-                <ul className={styles.List}>
-                    <li className={styles.Item}>
-                        React & Redux (каркас Create React App)
-                    </li>
-                    <li className={styles.Item}>TypeScript</li>
-                    <li className={styles.Item}>React Router</li>
-                    <li className={styles.Item}>SASS(SCSS)</li>
-                    <li className={styles.Item}>CSS Modules</li>
-                </ul>
-            </div>
-            <div className={styles.GitHub}>
-                <p>Исходный код (GitHub)</p>
-                <div className={styles.Button}>
+        <div className={styles.Wrapper}>
+            <div className={styles.Container}>
+                <div className={styles.Design}>
+                    <p>Задизайнено собственными ручками в Figma</p>
                     <Button
                         classesNames="Button Button_Yellow"
-                        href="https://github.com/kozlovtseva/personal"
+                        href="https://www.figma.com/file/L9ov2PqvFTtNATDYW35Yyq/personal?node-id=0%3A1"
                         text="посмотреть"
                     />
                 </div>
+                <div className={styles.Technologies}>
+                    <h5 className={styles.Title}>Что использовала:</h5>
+                    <ul className={styles.List}>
+                        <li className={styles.Item}>
+                            React & Redux (каркас Create React App)
+                        </li>
+                        <li className={styles.Item}>TypeScript</li>
+                        <li className={styles.Item}>React Router</li>
+                        <li className={styles.Item}>SASS(SCSS)</li>
+                        <li className={styles.Item}>CSS Modules</li>
+                    </ul>
+                </div>
+            </div>
+            <div className={styles.GitHub}>
+                <p>Исходный код (GitHub)</p>
+                <Button
+                    classesNames="Button Button_Yellow"
+                    href="https://github.com/kozlovtseva/personal"
+                    text="посмотреть"
+                />
             </div>
         </div>
     );
